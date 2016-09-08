beb4bff2014b203496bda3e1b54424ba723977dd
Jay Chetty <jaypaulchetty@gmail.com>
2016-09-08 11:55:53 +0100

    style maps

--- app/scripts/components/SiteMap.jsx
+++ app/scripts/components/SiteMap.jsx
@@ -4,6 +4,15 @@ import React from 'react'
 import GoogleMap from 'google-map-react';
 import SiteMarker from './SiteMarker.jsx'
 
+const createMapOptions = (maps)=>{
+    return {
+      // panControl: false,
+      // mapTypeControl: false,
+      // scrollwheel: false,
+      styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]
+    }
+  }
+
 const SiteMap = ( props )=>{
   const markers = props.sites.map((site)=>{
      return <SiteMarker name={site.name_en} key={site.unique_number} lat={site.latitude} lng={site.longitude} text={'A'} />
@@ -12,6 +21,7 @@ const SiteMap = ( props )=>{
   console.log('markers', markers)
   return(
     <GoogleMap
+     options= { createMapOptions }
      center={ {lat: props.center.latitude, lng: props.center.longitude} }
      zoom={5}>
      {markers}
