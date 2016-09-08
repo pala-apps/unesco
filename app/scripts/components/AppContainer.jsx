ac69804d2535320fdc5a250c99003b546b8ca687
Jay Chetty <jaypaulchetty@gmail.com>
2016-09-08 17:05:12 +0100

    adding images

--- app/scripts/components/AppContainer.jsx
+++ app/scripts/components/AppContainer.jsx
@@ -20,28 +20,21 @@ const AppContainer = ( props ) => {
     let request = new XMLHttpRequest();
     const tagArray = site.name_en.split(" ")
     const restrictedWords = ["and", "or", "the", "of", "in"]
-    console.log('tagArray', tagArray)
     const filteredTagArray = tagArray.filter((text)=>{
       const isRestrictedWord = restrictedWords.some((wordToRemove)=>{
         return wordToRemove === text
       })
       return !isRestrictedWord
     })
-    console.log('filteredTagArray', filteredTagArray)
     const tagString = filteredTagArray.join(',')
-    console.log('tagString', filteredTagArray)
-    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c1b8a45db98de911a8b6038483f5de85&safe_search=1&per_page=20&format=json&nojsoncallback=1&sort=interestingness-asc&tags=${tagString}&tag_mode=all`
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c1b8a45db98de911a8b6038483f5de85&safe_search=1&per_page=20&format=json&nojsoncallback=1&sort=interestingness-asc&tags=${tagString}&per_page=20&content_type=1&sort=relevance`
     request.open("GET", url)
     request.onload = function(e){
-      console.log('e', e)
-      console.log('image request', request.responseText)
       const imageDetails = JSON.parse( request.responseText );
       const filteredImageDetails = imageDetails.photos.photo.slice(0,4);
-      console.log('filtered images', filteredImageDetails);
       const imageUrls = filteredImageDetails.map((details)=>{
         return `https://farm${details.farm}.staticflickr.com/${details.server}/${details.id}_${details.secret}.jpg`
       })
-      console.log('imageUrls', imageUrls);
       props.dispatch( actions.addSiteImages( site.unique_number, imageUrls ) )
 
     }
