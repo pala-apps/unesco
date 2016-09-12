bf81afb3af2a68d42a9fa5917d8d2dd5192caece
Jay Chetty <jaypaulchetty@gmail.com>
2016-09-12 15:03:26 +0100

    more heroku

--- heroku-server.js
+++ heroku-server.js
@@ -3,5 +3,9 @@ var app = express();
 var Path = require('path');
 
 
+app.get( '/', function( req, res ) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
 app.use(express.static( Path.join(__dirname, 'public' )));
 var server = app.listen( process.env.PORT || 3333);
