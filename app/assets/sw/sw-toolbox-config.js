3916fa59e89d72f72d3fd70fb51197733172b6cf
Jay Chetty <jaypaulchetty@gmail.com>
2016-09-14 16:47:24 +0100

    cache flickr images

--- app/assets/sw/sw-toolbox-config.js
+++ app/assets/sw/sw-toolbox-config.js
@@ -9,3 +9,13 @@ toolbox.router.get('/(.*)', toolbox.cacheFirst, {
   origin: "https://fonts.gstatic.com",
   cache: {name: 'font-cache'}
 })
+
+toolbox.router.get('/services/rest', toolbox.cacheFirst, {
+  origin: "https://api.flickr.com",
+  cache: {name: 'flickr-cache'}
+})
+
+toolbox.router.get('/(.*)', toolbox.cacheFirst, {
+  origin: /^https:\/\/farm[0-9]*.staticflickr.com/,
+  cache: {name: 'flickr-cache'}
+})
